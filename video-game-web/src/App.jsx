36a8802c9e993a1a31d955e0f8972d0eb6c7adf3
Nicholas Gonzalez <nichoja20@gmail.com>
2025-07-31 10:12:17 -0400

    react setup

---
+++ video-game-web/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header.jsx'
+import um from '../../../'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
