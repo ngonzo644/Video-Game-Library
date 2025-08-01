dc8a9d8aaf7799ad01dee44face5c7dab932e8c5
Nicholas Gonzalez <nichoja20@gmail.com>
2025-07-31 23:23:36 -0400

    dd

--- video-game-web/src/App.jsx
+++ video-game-web/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header.jsx'
+import {useEffect, useState} from 'react'
 
 
 function App() {
+
+  useEffect( ()=>{
   const getGames = async ()=>{
     const res = await fetch('http://localhost:3000/games', {
       method:'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query: 'fields name; limit 10;'})
+      body: JSON.stringify({query: `search "Halo"; 
+      cover.url, first_release_date, genres.name, rating; limit 5;`})
 
     });
 
@@ -18,6 +21,7 @@ function App() {
   }
 
   getGames();
+}, []);
 
 
   return (
