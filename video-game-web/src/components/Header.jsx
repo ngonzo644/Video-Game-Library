c130973e6b32f9c772f17ac68f70f9cdc40eb692
Nicholas Gonzalez <nichoja20@gmail.com>
2025-08-02 11:53:05 -0400

    made search bar interative

--- video-game-web/src/components/Header.jsx
+++ video-game-web/src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {useState} from 'react'
 
-const Header = () => {
+const Header = ({query, setQuery}) => {
+
+  const gameInput = (vg)=>{
+    setQuery(vg.target.value);
+  }
+
   return (
     <>
       <div className='text-center flex flex-row justify-between bg-black p-10'>
@@ -8,7 +14,7 @@ const Header = () => {
             hello
         </div>
         <div className='flex-1 text-center text-white'>
-          <input type="text" className='border-solid border-white text-black rounded bg-white' placeholder='Search'>
+          <input type="text" className='border-solid border-white text-black rounded bg-white' placeholder='Search' value={query} onChange={gameInput}>
           </input>
         </div>
         <div className='flex text-center text-white shrink'>
