13eb6c9347c2c85a83694845801504d38ea84d28
Nicholas Gonzalez <nichoja20@gmail.com>
2025-09-04 23:16:36 -0400

    made distinct frontend and backend folders

---
+++ video-game-web/frontend/src/components/Body.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Ring} from 'ldrs/react'
+import 'ldrs/react/Ring.css'
+import Card from './Card.jsx'
+import { BackgroundGradientAnimation } from "./ui/background-gradient-animation.jsx";
+
+const Body = ({trend, goat, fps}) => {
+  return (
+    <>
+
+      <div className= "flex flex-col pt-32 space-y-10 pl-3 overflow-auto bg-gradient-to-r from-gray-500 via-gray-400 to-gray-700 pb-200">
+      <Card title="Trending Games" vg={trend} />
+      <Card title="Must Plays" vg={goat}/>
+      <Card title="Iconic Shooters" vg={fps}/>
+      </div>
+    </>
+  )
+}
+
+export default Body
